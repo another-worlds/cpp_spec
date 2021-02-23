4f465bdc7a645ff472e2a3e68e9e1b936cd8528e
another-worlds <a.egorshev@gmail.com>
2021-02-23 12:45:08 +0300

    algo block complete

--- white/week3/algo/tolower/main.cpp
+++ white/week3/algo/tolower/main.cpp
@@ -11,19 +11,29 @@ int main() {
     int q;
     cin >> q;
     string str;
-    vector<char> vec;
+    vector<string> vec;
     
 
     for (int i = 0; i < q; i++)
     {
-        char ch;
+        string ch;
         cin >> ch;
         vec.push_back(ch);
     }
 
     
-    sort(begin(vec), end(vec), [](int x, int y) { 
-        return tolower(x) < tolower(y);
+    sort(begin(vec), end(vec), [](string x, string y) {
+        string x1, y1;
+        x1 = x; 
+        y1 = y;
+        for(int l = 0; l < x1.size(); l++) {
+            x1[l] = tolower(x1[l]);
+        }
+        for(int k = 0; k < y1.size(); k++) {
+            y1[k] = tolower(y1[k]);
+        }
+        
+        return x1 < y1;
     });
 
     for (int j = 0; j < q; j++)
