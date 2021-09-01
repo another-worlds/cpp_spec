c3850c32a2cb6efb91ccf0fa72090a8bface15d9
another-worlds <a.egorshev@gmail.com>
2021-09-01 17:56:11 +0300

    fixed to revFuntion

--- white/week4/revFunctions/main.cpp
+++ white/week4/revFunctions/main.cpp
@@ -5,13 +5,25 @@
 #include <algorithm>
 using namespace std;
 
-struct Element {
+class Element {
 public:
 	Element(char op, double val) {
 		operation = op;
 		value = val;
 	}
 
+	double Apply(double source_value) const {
+		if (operation == '+') {
+			return source_value + value;
+		} else {
+			return source_value - value;
+		}
+	}
+	void Invert() {
+		if (operation == '+') { operation = '-'; } else { operation = '+'; }
+	}
+
+private:
 	char operation;
 	double value;
 };
@@ -30,67 +42,17 @@ public:
 
 	double Apply(double value) const {
 		double result = value;
-		for(int i = 0; i < elements.size(); i++) {
-			result = execute(result, elements[i]);
-		}
+		for(const Element& el: elements) { result = el.Apply(result); }
 		return result;
 	}
-
+ 
 	void Invert() {
+		for(auto& el: elements) {
+			el.Invert();
+		}
 		reverse(elements.begin(), elements.end());
 	}
 
 private:
-	double execute(double result, Element el) const {
-		char op = el.operation;
-		double val = el.value;
-		if(op == '+') {
-			return result + val;
-		}
-		else {
-			return result - val;
-		}
-	}
 	vector<Element> elements;
 };
-
-struct Image {
-  double quality;
-  double freshness;
-  double rating;
-};
-
-struct Params {
-  double a;
-  double b;
-  double c;
-};
-
-Function MakeWeightFunction(const Params& params,
-                            const Image& image) {
-  Function function;
-  function.AddPart('-', image.freshness * params.a + params.b);
-  function.AddPart('+', image.rating * params.c);
-  return function;
-}
-
-double ComputeImageWeight(const Params& params, const Image& image) {
-  Function function = MakeWeightFunction(params, image);
-  return function.Apply(image.quality);
-}
-
-double ComputeQualityByWeight(const Params& params,
-                              const Image& image,
-                              double weight) {
-  Function function = MakeWeightFunction(params, image);
-  function.Invert();
-  return function.Apply(weight);
-}
-
-int main() {
-  Image image = {10, 2, 6};
-  Params params = {4, 2, 6};
-  cout << ComputeImageWeight(params, image) << endl;
-  cout << ComputeQualityByWeight(params, image, 46) << endl;
-  return 0;
-}
