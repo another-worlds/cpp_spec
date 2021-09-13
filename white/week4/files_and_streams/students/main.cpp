479ce6ed3dbaf7344e83ed9be015c840c4681bb9
another-worlds <a.egorshev@gmail.com>
2021-09-13 15:24:10 +0300

    modified student template

--- white/week4/files_and_streams/students/main.cpp
+++ white/week4/files_and_streams/students/main.cpp
@@ -9,10 +9,13 @@ using namespace std;
 struct Student {
 public:
     explicit Student(string f_n, string s_n,
-                     int d, int m, int y) {
+                     int d, int m, int y, int i) {
         first_name  = f_n;
         second_name = s_n;
-        day = 
+        day = d;
+        month = m;
+        year = y;
+        index = i;
     }
 
 
@@ -21,18 +24,49 @@ public:
     int day;
     int month;
     int year;
+    int index;
+};
+
+string process_query(int index, string query, vector<Student> students) {
+    if(query == "date") {
+        //smth
+    } else if (query == "name")
 }
 
-
 int main() {
-
-    int n;
-
+    vector<Student> students;
+    int n, m;
     cin >> n;
 
     for(int i = 0; i < n; i++) {
+        //read name and surame
+        string f_n, s_n;
+        cin >> f_n >> s_n;
+        
+        //read day, month and year
+        int d, m, y;
+        cin >> d >> m >> y;
 
-
+        //create corresponding object
+        Student student(f_n, s_n, d, m, y, i + 1);
+        students.push_back(student);
     }
+
+    //read number of queries
+    cin >> m;
+
+    for(int i = 0; i < m; i++) {
+        //read type of query
+        string query;
+        cin >> query;
+
+        //read object of query
+        int index;
+        cin >> index;
+
+        //TODO:output query results
+        process_query(index, query, students);
+    }
+
     return 0;
 }
